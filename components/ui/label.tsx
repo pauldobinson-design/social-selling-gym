da7753b6b6fc002f65b6dfdcdef41c1a9b031360
pauldobinson-design <pauldobinson@gmail.com>
2025-10-21 13:59:52 +1100

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,24 +1,9 @@
-'use client'
+// components/ui/label.tsx
+import * as React from "react";
 
-import * as React from 'react'
-import * as LabelPrimitive from '@radix-ui/react-label'
+type LabelProps = React.LabelHTMLAttributes<HTMLLabelElement>;
 
-import { cn } from '@/lib/utils'
-
-function Label({
-  className,
-  ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
-  return (
-    <LabelPrimitive.Root
-      data-slot="label"
-      className={cn(
-        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
-        className,
-      )}
-      {...props}
-    />
-  )
+export function Label({ className = "label", ...props }: LabelProps) {
+  return <label className={className} {...props} />;
 }
-
-export { Label }
+export default Label;
